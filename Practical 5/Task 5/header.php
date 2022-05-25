3b58ba9cf756f1adbe5cd5b7cc24a757069832a3
scrub-lord-ux <102908326+Adrian-Hawkins@users.noreply.github.com>
2022-05-25 12:23:40 +0200

    Finished user login and header for part 1 of 6

---
+++ Practical 5/Task 5/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Sports Statistics</title>
+        <link rel="stylesheet" type="text/css" href="css\header.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="#">Add Player</a></li>
+                    <li><a href="#">Add Event</a></li>
+                    <li><a href="#">Manage Event</a></li>
+                    <li><a href="#">View Statistics</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
---
+++ Practical 5/Task 5/index.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    echo '<div class="text-center">Want to Leave the Page? <br><br><br><a href="logout.php">Logout</a></div>';
+</body>
+</html>
